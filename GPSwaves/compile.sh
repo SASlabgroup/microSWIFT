ce7d9eae469417015ae20f72d1dd82a307b4f0ee
alexdeklerk <adeklerk@apl.washington.edu>
2021-01-14 01:35:30 +0000

    recompile GPSwavesC and update compile.sh

--- GPSwaves/compile.sh
+++ GPSwaves/compile.sh
@@ -1,7 +1,7 @@
 #
 g++ -c -fPIC -c *.c  -ggdb -I/usr/include/python3.7 -L/usr/lib/python3.7 -I/usr/local/include/boost -L/usr/local/lib/boost -I/usr/local/include/boost/python -L/usr/local/lib/boost/python 
 
-g++ -fPIC -shared -Wl,-soname,GPSwavesC.so -ggdb -o GPSwavesC.so  -Wno-undef -I/home/kay/Desktop/ETEINEUROPE/raspberrypi/GPSwaves *.o -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lm -g -I/usr/local/include/boost  -L/usr/local/lib/boost  -I/usr/include/python3.7 -I/usr/local/include/boost/python -L/usr/local/lib/boost/python -lboost_python37 -lboost_numpy37
+g++ -fPIC -shared -Wl,-soname,GPSwavesC.so -ggdb -o GPSwavesC.so  -Wno-undef -I/home/pi/GPSwaves *.o -I/usr/include/x86_64-linux-gnu -L/usr/lib/x86_64-linux-gnu -lm -g -I/usr/local/include/boost  -L/usr/local/lib/boost  -I/usr/include/python3.7 -I/usr/local/include/boost/python -L/usr/local/lib/boost/python -lboost_python37 -lboost_numpy37
 
 
 
