18f4f7a53326c0d5ffbe8d430c2278501d1b4a48
Alex de Klerk <adeklerk@apl.washington.edu>
2021-01-22 01:06:39 +0000

    Update recordImu.py
    remove pitch, roll, yaw calculation from recordIMU.py

--- sensors/recordImu.py
+++ sensors/recordImu.py
@@ -121,10 +121,6 @@ while True:
                 except Exception as e:
                     logger.info(e)
                     logger.info('error reading IMU data')
-
-                roll = 180 * math.atan(accel_x/math.sqrt(accel_y*accel_y + accel_z*accel_z))/math.pi
-                pitch = 180 * math.atan(accel_y/math.sqrt(accel_x*accel_x + accel_z*accel_z))/math.pi
-                yaw = 180 * math.atan(accel_z/math.sqrt(accel_x*accel_x + accel_y   *accel_y))/math.pi
          
                 timestamp='{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
 
