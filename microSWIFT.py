4656bd09df4aab72b11299914254919b5c1ffedd
edwinrainville <erainvil@uw.edu>
2021-07-26 17:01:28 -0700

    microSWIFT.py is created with psuedo-code outline

---
+++ microSWIFT.py
@@ -0,0 +1,27 @@
+## microSWIFT.py 
+"""
+author: @erainvil
+
+Description: This script is the main operational script that runs on the microSWIFT. It is the scheduler that runs the recording of the GPS
+and IMU as well as schedules the processing scripts after they are done recording. 
+
+
+"""
+
+# Main import Statemennts
+import numpy as np
+
+# Boot up and get microSWIFT characteristics
+
+# Run recordGPS.py and recordIMU.py concurrently
+
+# Run processData.py 
+
+# Create TX file from processData.py 
+
+# Send SBD
+
+# restart while it is still on
+
+
+
