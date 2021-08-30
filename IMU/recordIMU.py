cd30b12bf32b202ae9d71560512aba472dab2d39
EJ Rainville <erainvil@uw.edu>
2021-08-30 13:40:14 -0700

    Refactored recordIMU to match the same structure as recordGPS (got rid of nested functions)

--- IMU/recordIMU.py
+++ IMU/recordIMU.py
@@ -17,110 +17,66 @@ from time import sleep
 # Raspberry pi inputs
 import RPi.GPIO as GPIO
 
-# Configuration imports
-from utils.config3 import Config
-
 # IMU sensor imports
 import IMU.adafruit_fxos8700_microSWIFT
 import IMU.adafruit_fxas21002c_microSWIFT
+
+# Configuration imports
+from utils.config3 import Config
+configFilename = r'utils/Config.dat'
+
+# System and logging parameters
+config = Config() # Create object and load file
+ok = config.loadFile( configFilename )
+if( not ok ):
+    sys.exit(0)
+
+# Set up module level logger
+logger = getLogger('microSWIFT.'+__name__)  
+
+#system parameters 
+floatID = os.uname()[1]
+dataDir = config.getString('System', 'dataDir')
+burst_interval=config.getInt('System', 'burst_interval')
+burst_time=config.getInt('System', 'burst_time')
+burst_seconds=config.getInt('System', 'burst_seconds')
+bad = config.getInt('System', 'badValue')
+
+#IMU parameters
+imuFreq=config.getFloat('IMU', 'imuFreq')
+imu_samples = imuFreq*burst_seconds
+imu_gpio=config.getInt('IMU', 'imu_gpio')
+
+#initialize IMU GPIO pin as modem on/off control
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(imu_gpio,GPIO.OUT)
+#turn IMU on for script recognizes i2c address
+GPIO.output(imu_gpio,GPIO.HIGH)
 
 
 def recordIMU(end_time):
     # IMU is not Initialzied at first
     imu_initialized = False
 
+    # Loop if the imu did not initialize and it is still within a recording block
     while datetime.utcnow().minute + datetime.utcnow().second/60 < end_time and imu_initialized==False:
-
-        ## --------- Define Initialize Function --------------
-        def init():
-            #initialize fxos and fxas devices (required after turning off device)
-            logger.info('power on IMU')
-            GPIO.output(imu_gpio,GPIO.HIGH)
-            i2c = busio.I2C(board.SCL, board.SDA)
-            fxos = IMU.adafruit_fxos8700_microSWIFT.FXOS8700(i2c, accel_range=0x00)
-            fxas = IMU.adafruit_fxas21002c_microSWIFT.FXAS21002C(i2c, gyro_range=500)
-
-            # Sleep to start recording at same time as GPS
-            sleep(5.1)
-            
-            # return initialized values
-            imu_initialized = True
-            return fxos, fxas, imu_initialized
-
-        ## ---------- Define Record Function -------------------
-        def record(IMUdataFilename):
-            logger.info('starting IMU burst at {}'.format(datetime.now()))
-            
-            # Open the new IMU data file for logging
-            with open(IMUdataFilename, 'w',newline='\n') as imu_out:
-                logger.info('open file for writing: {}'.format(IMUdataFilename))
-                t_end = time.time() + burst_seconds #get end time for burst
-                isample=0
-                while isample < imu_samples:
-                    # Get values from IMU
-                    try:
-                        accel_x, accel_y, accel_z = fxos.accelerometer
-                        mag_x, mag_y, mag_z = fxos.magnetometer
-                        gyro_x, gyro_y, gyro_z = fxas.gyroscope
-                    except Exception as e:
-                        logger.info(e)
-                        logger.info('error reading IMU data')
-
-                    # Get current timestamp
-                    timestamp='{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
-
-                    # Write data and timestamp to file
-                    imu_out.write('%s,%f,%f,%f,%f,%f,%f,%f,%f,%f\n' %(timestamp,accel_x,accel_y,accel_z,mag_x,mag_y,mag_z,gyro_x,gyro_y,gyro_z))
-                    imu_out.flush()
-                    
-                    # Index up number of samples
-                    isample = isample + 1
-
-                    # hard coded sleep to control recording rate. NOT ideal but works for now    
-                    sleep(0.065)
-                
-                # End of IMU sampling
-                logger.info('end burst')
-                logger.info('IMU samples {}'.format(isample)) 
-                logger.info('IMU ending burst at: {}'.format(datetime.now()))
-
-                # Turn IMU Off   
-                GPIO.output(imu_gpio,GPIO.LOW)
-                logger.info('power down IMU')
-            
-        ## ------------ Main Body of Function ------------------
-
-        # System and logging parameters
-        config = Config() # Create object and load file
-        ok = config.loadFile( configFilename )
-        if( not ok ):
-            sys.exit(0)
-
-        # Set up module level logger
-        logger = getLogger('microSWIFT.'+__name__)  
-
-        #system parameters 
-        floatID = os.uname()[1]
-        dataDir = config.getString('System', 'dataDir')
-        burst_interval=config.getInt('System', 'burst_interval')
-        burst_time=config.getInt('System', 'burst_time')
-        burst_seconds=config.getInt('System', 'burst_seconds')
-        bad = config.getInt('System', 'badValue')
-
-        #IMU parameters
-        imuFreq=config.getFloat('IMU', 'imuFreq')
-        imu_samples = imuFreq*burst_seconds
-        imu_gpio=config.getInt('IMU', 'imu_gpio')
-
-        #initialize IMU GPIO pin as modem on/off control
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(imu_gpio,GPIO.OUT)
-        #turn IMU on for script recognizes i2c address
-        GPIO.output(imu_gpio,GPIO.HIGH)
-
+        
         ## --------------- Initialize IMU -----------------
         logger.info('initializing IMU')
-        fxos, fxas, imu_initialized = init()
+
+        # initialize fxos and fxas devices (required after turning off device)
+        logger.info('power on IMU')
+        GPIO.output(imu_gpio,GPIO.HIGH)
+        i2c = busio.I2C(board.SCL, board.SDA)
+        fxos = IMU.adafruit_fxos8700_microSWIFT.FXOS8700(i2c, accel_range=0x00)
+        fxas = IMU.adafruit_fxas21002c_microSWIFT.FXAS21002C(i2c, gyro_range=500)
+
+        # Sleep to start recording at same time as GPS
+        sleep(5.1)
+        
+        # return initialized values
+        imu_initialized = True
+
         logger.info('IMU initialized')
 
         ## --------------- Record IMU ----------------------
@@ -128,7 +84,44 @@ def recordIMU(end_time):
         logger.info('---------------recordIMU.py------------------')
         IMUdataFilename = dataDir + floatID + '_IMU_'+'{:%d%b%Y_%H%M%SUTC.dat}'.format(datetime.utcnow())
         logger.info('file name: {}'.format(IMUdataFilename))
-        record(IMUdataFilename)
+        logger.info('starting IMU burst at {}'.format(datetime.now()))
+            
+        # Open the new IMU data file for logging
+        with open(IMUdataFilename, 'w',newline='\n') as imu_out:
+            logger.info('open file for writing: {}'.format(IMUdataFilename))
+            t_end = time.time() + burst_seconds #get end time for burst
+            isample=0
+            while isample < imu_samples:
+                # Get values from IMU
+                try:
+                    accel_x, accel_y, accel_z = fxos.accelerometer
+                    mag_x, mag_y, mag_z = fxos.magnetometer
+                    gyro_x, gyro_y, gyro_z = fxas.gyroscope
+                except Exception as e:
+                    logger.info(e)
+                    logger.info('error reading IMU data')
+
+                # Get current timestamp
+                timestamp='{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
+
+                # Write data and timestamp to file
+                imu_out.write('%s,%f,%f,%f,%f,%f,%f,%f,%f,%f\n' %(timestamp,accel_x,accel_y,accel_z,mag_x,mag_y,mag_z,gyro_x,gyro_y,gyro_z))
+                imu_out.flush()
+                
+                # Index up number of samples
+                isample = isample + 1
+
+                # hard coded sleep to control recording rate. NOT ideal but works for now    
+                sleep(0.065)
+            
+            # End of IMU sampling
+            logger.info('end burst')
+            logger.info('IMU samples {}'.format(isample)) 
+            logger.info('IMU ending burst at: {}'.format(datetime.now()))
+
+            # Turn IMU Off   
+            GPIO.output(imu_gpio,GPIO.LOW)
+            logger.info('power down IMU')
 
         # Return IMUdataFilename to main microSWIFT.py
         return IMUdataFilename, imu_initialized
