92e0ed2d6a008d20f9e4b1761ec72a4ef577de09
edwinrainville <erainvil@uw.edu>
2022-04-28 16:49:48 -0700

    changes all spaces to tabs - weird problems

--- microSWIFT.py
+++ microSWIFT.py
@@ -53,21 +53,21 @@ from SBD.sendSBD import send_microSWIFT_51
 from utils.config3 import Config
 
 def _get_uvzmean(badValue, pts):
-    mean = badValue     #set values to 999 initially and fill if valid value
-    index = np.where(pts != badValue)[0] #get index of non bad values
-    pts=pts[index] #take subset of data without bad values in it
-    
+	mean = badValue     #set values to 999 initially and fill if valid value
+	index = np.where(pts != badValue)[0] #get index of non bad values
+	pts=pts[index] #take subset of data without bad values in it
+	
 	if(len(index) > 0):
-        mean = np.mean(pts)
-    
+		mean = np.mean(pts)
+   
 	return mean
 
 def _get_last(badValue, pts):
-    for i in range(1, len(pts)): #loop over entire lat/lon array
+	for i in range(1, len(pts)): #loop over entire lat/lon array
 		if pts[-i] != badValue: #count back from last point looking for a real position
-            return pts[-i]
-        
-    return badValue #returns badValue if no real position exists
+			return pts[-i]
+		
+	return badValue #returns badValue if no real position exists
 
 # Main body of microSWIFT.py
 if __name__=="__main__":
