5f1bc23f1318cdb11700b22143900af7d380a895
edwinrainville <erainvil@uw.edu>
2022-05-17 16:30:46 -0700

    Flipped the sending message order to be last in first out (LIFO) so that the most recent message is sent first

--- microSWIFT.py
+++ microSWIFT.py
@@ -262,18 +262,20 @@ if __name__=="__main__":
 			# Append the telemetrry queue with the processed data
 			logger.info('Adding TX filename to the telemetry queue')
 			telemetryQueue = open('/home/pi/microSWIFT/SBD/telemetryQueue.txt','a')
-			telemetryQueue.write(TX_fname)
-			telemetryQueue.write('\n')
+			telemetryQueue.write(TX_fname + '\n')
 			telemetryQueue.close()
 
 			# Send as many payloads as possible from the queue in FIFO order
 			telemetryQueue = open('/home/pi/microSWIFT/SBD/telemetryQueue.txt','r+')
-			payload_files = telemetryQueue.readlines()
-			logger.info('Number of Messages to send: {}'.format(len(payload_files)))
+			payload_files_FIFO = telemetryQueue.readlines()
+			payload_files_LIFO = payload_files_FIFO.reverse()
+			logger.info(payload_files_FIFO)
+			logger.info(payload_files_LIFO)
+			logger.info('Number of Messages to send: {}'.format(len(payload_files_FIFO)))
 
 			# Send as many messages from the queue as possible during the send window
 			messages_sent = 0
-			for TX_file in payload_files:
+			for TX_file in payload_files_LIFO:
 				# Check if we are still in the send window 
 				if datetime.utcnow() < next_start - timedelta(seconds=10):
 					# Open the TX file for the payload 
@@ -297,17 +299,16 @@ if __name__=="__main__":
 
 			# Log the send statistics
 			logger.info('Messages Sent: {}'.format(int(messages_sent)))
-			messages_remaining = int(len(payload_files)) - messages_sent
+			messages_remaining = int(len(payload_files_LIFO)) - messages_sent
 			logger.info('Messages Remaining: {}'.format(messages_remaining))
 
 			# Remove the sent messages from the queue by writing the remaining lines to the file
 			if messages_remaining > 0:
 				telemetryQueue.seek(0)
-				for n in np.arange(messages_sent, len(payload_files)):
+				for n in np.arange(messages_remaining):
 					logger.info('Writing line {}'.format(n))
-					logger.info(payload_files[n])
-					telemetryQueue.write(payload_files[n])
-					telemetryQueue.write('\n')
+					logger.info(payload_files_FIFO[n])
+					telemetryQueue.write(payload_files_FIFO[n] + '\n')
 			else:
 				# Empty all the lines from the file if all messages were sent
 				telemetryQueue.truncate()
