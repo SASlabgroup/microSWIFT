2d3ab1ae60d079143215170feb749c809bc37009
edwinrainville <erainvil@uw.edu>
2022-05-17 17:04:21 -0700

    Put the new line in front of appending the new file name

--- microSWIFT.py
+++ microSWIFT.py
@@ -262,8 +262,8 @@ if __name__=="__main__":
 			# Append the telemetrry queue with the processed data
 			logger.info('Adding TX filename to the telemetry queue')
 			telemetryQueue = open('/home/pi/microSWIFT/SBD/telemetryQueue.txt','a')
-			telemetryQueue.write(TX_fname)
 			telemetryQueue.write('\n')
+			telemetryQueue.write(TX_fname)
 			telemetryQueue.close()
 
 			# Send as many payloads as possible from the queue in FIFO order
