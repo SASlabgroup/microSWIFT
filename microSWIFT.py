b12407a105f345cb308ed208638a21a76932f752
edwinrainville <erainvil@uw.edu>
2022-05-17 17:00:15 -0700

    added in a new line to the telemetry Queue when the file name is first appended

--- microSWIFT.py
+++ microSWIFT.py
@@ -263,6 +263,7 @@ if __name__=="__main__":
 			logger.info('Adding TX filename to the telemetry queue')
 			telemetryQueue = open('/home/pi/microSWIFT/SBD/telemetryQueue.txt','a')
 			telemetryQueue.write(TX_fname)
+			telemetryQueue.write('\n')
 			telemetryQueue.close()
 
 			# Send as many payloads as possible from the queue in FIFO order
