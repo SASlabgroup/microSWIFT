42c560604d24f52224a1b40a3b39c0aa261507fa
edwinrainville <erainvil@uw.edu>
2022-05-17 17:11:10 -0700

    Try only having a new line command in the write functions not when appending the file name

--- microSWIFT.py
+++ microSWIFT.py
@@ -262,7 +262,7 @@ if __name__=="__main__":
 			# Append the telemetrry queue with the processed data
 			logger.info('Adding TX filename to the telemetry queue')
 			telemetryQueue = open('/home/pi/microSWIFT/SBD/telemetryQueue.txt','a')
-			telemetryQueue.write('\n')
+			# telemetryQueue.write('\n')
 			telemetryQueue.write(TX_fname)
 			telemetryQueue.close()
 
