2d5d310f84185fe7cb3923c387b1c53e85d124a6
jacobrdavis <jacobdavis4085@gmail.com>
2022-08-09 16:56:11 -0700

    'collate debugging'

--- waves/collateIMUandGPS.py
+++ waves/collateIMUandGPS.py
@@ -37,6 +37,8 @@ def datetimearray2relativetime(datetimeArr,t0):
     Outputs:
         relTime - 
     """
+    print(t0)
+    print((datetimeArr-t0))
     relTime = [timestep.total_seconds() for timestep in (datetimeArr-t0)]
     return relTime   
 
