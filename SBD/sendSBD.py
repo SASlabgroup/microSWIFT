5295362f273a8933819d7e5379d855eb387da64d
jacobrdavis <jacobdavis4085@gmail.com>
2022-08-15 16:21:35 -0700

    'fix tx date'

--- SBD/sendSBD.py
+++ SBD/sendSBD.py
@@ -136,11 +136,10 @@ def createTX(Hs, Tp, Dp, E, f, a1, b1, a2, b2, check, u_mean, v_mean, z_mean, la
 
 
     #Create file name
-    TX_fname = dataDir + floatID+'_TX_'+"{:%d%b%Y_%H%M%SUTC}_2.dat}".format(now) 
 
-    # TX_fname = dataDir + floatID+'_TX_'+"{:%d%b%Y_%H%M%SUTC.dat}".format(now) 
-    # if os.path.exists(TX_fname): # support secondary estimate
-    #     TX_fname = dataDir + floatID+'_TX_'+"{:%d%b%Y_%H%M%SUTC}_2.dat}".format(now) 
+    TX_fname = dataDir + floatID+'_TX_'+"{:%d%b%Y_%H%M%SUTC.dat}".format(now) 
+    if os.path.exists(TX_fname): # support secondary estimate
+        TX_fname = dataDir+floatID+'_TX_'+"{:%d%b%Y_%H%M%SUTC}}".format(now)+'_2.dat'
 
     # Open the TX file and start to write to it    
     with open(TX_fname, 'wb') as file:
