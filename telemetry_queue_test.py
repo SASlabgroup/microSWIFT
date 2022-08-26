362a46086d4833e49b67550980d7a2eadfe21b11
jacobrdavis <jacobdavis4085@gmail.com>
2022-08-26 12:55:45 -0700

    'update test file'

--- telemetry_queue_test.py
+++ telemetry_queue_test.py
@@ -13,7 +13,7 @@ TX_file = '/home/pi/microSWIFT/data/microSWIFT043_TX_26Aug2022_195204UTC.dat'
 with open(TX_file, mode='rb') as file: # b is important -> binary
 						payload_data = file.read()
 					# logger.info(f'Opened TX')
-
+print(payload_data)
 # read in the sensor type from the binary payload file
 payloadStartIdx = payload_data.index(b':') 
 
