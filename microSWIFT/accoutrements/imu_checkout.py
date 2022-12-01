6920107ae9dd62b574d174e41dcd81fa65d416dd
cranders-bio <cander20@uw.edu>
2022-12-01 12:53:05 -0800

    added new test

--- microSWIFT/accoutrements/imu_checkout.py
+++ microSWIFT/accoutrements/imu_checkout.py
@@ -1,3 +1,16 @@
 """
 Functions to test the installation, calibration, and validity of the onboard IMU.
 """
+import unittest
+
+from utils import configuration
+
+class Testimu_checkout(unittest.TestCase):
+    """
+
+    """
+    def test_smoke(self):
+        """
+
+        """
+        1=1
