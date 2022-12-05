46ee311bfb153f162caf674639bf63a43ac6a98a
edwinrainville <erainvil@uw.edu>
2022-12-05 14:58:33 -0800

    Removes configuration dependencies from log init

--- microSWIFT/utils/log.py
+++ microSWIFT/utils/log.py
@@ -1,26 +1,9 @@
 """
 MicroSWIFT-specific logging utilities.
-
-#TODO:
-    - fix the configuration import
 """
 
 import logging
 import math
-import sys
-
-from config import Config
-
-
-# Define Config file name and load file
-CONFIG_FILENAME = r'/home/pi/microSWIFT/utils/Config.dat'
-config = Config() # Create object and load file
-loaded = config.loadFile(CONFIG_FILENAME)
-if not loaded:
-    print("Error loading config file")
-    sys.exit(1)
-
-LOG_HEADER_LENGTH = 50 #TODO: put this in the config
 
 def init() -> logging.Logger:
     """
@@ -32,21 +15,20 @@ def init() -> logging.Logger:
     logger = logging.getLogger('microSWIFT')
 
     # Get log parameters from configuration:
-    LOG_DIR = config.getString('Loggers', 'logDir')
-    LOG_LEVEL = config.getString('Loggers', 'DefaultLogLevel')
+    LOG_FILE_NAME = '/home/pi/microSWIFT/logs/microSWIFT.log'
+    LOG_LEVEL = 'INFO'
     LOG_FORMAT = ('%(asctime)s, %(name)s - [%(levelname)s] - %(message)s')
-    LOG_FILE = (LOG_DIR  + logger.name + '.log')
 
     # Set log parameters:
     logger.setLevel(LOG_LEVEL)
-    log_file_handler = logging.FileHandler(LOG_FILE)
+    log_file_handler = logging.FileHandler(LOG_FILE_NAME)
     log_file_handler.setLevel(LOG_LEVEL)
     log_file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
     logger.addHandler(log_file_handler)
 
     return logger
 
-def header(header_name: str, length: int = LOG_HEADER_LENGTH) -> str:
+def header(header_name: str, length: int = 50) -> str:
     """
     Utility to create uniform length log headers with a function name.
 
@@ -61,6 +43,5 @@ def header(header_name: str, length: int = LOG_HEADER_LENGTH) -> str:
     Example:
         log_header(__name__, length = 50)
     """
-    header_name = header_name
     header_line_length = math.floor((length-len(header_name))/2)
     return header_line_length*'-' + header_name + header_line_length*'-'
