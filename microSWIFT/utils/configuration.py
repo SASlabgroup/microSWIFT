da049a6d3eed357951bb0d91f209a07dc40cc96d
edwinrainville <erainvil@uw.edu>
2022-12-05 17:43:53 -0800

    Integrates config object throughout microSWIFT.py.
    Fixes all confusing and redundant time variables. Now all times
    are based on the closest start time, duty cycle length and record
    window length.

--- microSWIFT/utils/configuration.py
+++ microSWIFT/utils/configuration.py
@@ -3,18 +3,23 @@ Definition of the configuration class for the microSWIFT. The
 configuration will read in a few variables from the 
 """
 
+from datetime import datetime, timedelta
+import logging
+import os
 import warnings
+
+from . import log
 
 class Config:
     """
-    Class object for configuration of the microSWIFT. 
+    Class object for configuration of the microSWIFT.
     """
     def __init__(self, config_fname: str):
         """
         Initialize the configuration of the microSWIFT with all
         settings. The settings include values that really should not
         be changed and are hardcoded into the class and parameters that
-        can be regularly changed from the config.txt file. 
+        can be regularly changed from the config.txt file.
 
         Parameters
         ----------
@@ -27,11 +32,12 @@ class Config:
         config : class
             A class that contains all the parameters for the microSWIFT
             configuration including user changeable settings and
-            hardcoded settings. 
+            hardcoded settings.
         """
-    
-        (duty_cycle_length, 
-         record_window_length, 
+        logger = logging.getLogger('microSWIFT')
+
+        (duty_cycle_length,
+         record_window_length,
          gps_sampling_frequency,
          imu_sampling_frequency) = self.read_config_file(config_fname)
 
@@ -45,7 +51,7 @@ class Config:
         if (duty_cycle_length - record_window_length) < 5:
             warnings.warn('send and process window SHOULD be longer than 5'
                           ' minutes')
-        
+                         
         if gps_sampling_frequency >=1 and gps_sampling_frequency <= 4:
             pass
         else:
@@ -56,9 +62,29 @@ class Config:
         else:
             raise ValueError('imu_sampling_frequency is out of range')
 
+        self.DUTY_CYCLES_PER_HOUR = int(60/duty_cycle_length)
+        self.DUTY_CYCLE_LENGTH = timedelta(minutes=duty_cycle_length)
+        self.RECORD_WINDOW_LENGTH = timedelta(minutes=record_window_length)
+        get_record_window_start_time(self)
+        self.GPS_SAMPLING_FREQ = gps_sampling_frequency
+        self.IMU_SAMPLING_FREQ = imu_sampling_frequency     
+       
         # System Parameters
-        
+        self.ID = os.uname()[1]
+        self.PAYLOAD_TYPE = 7
+        self.SENSOR_TYPE = 52
 
+        # Data Parameters
+        self.WAVE_PROCESSING_TYPE = 'gps_waves'
+        self.BAD_VALUE = 999
+        self.NUM_COEF = 42
+
+        logger.info(log.header(''))
+        logger.info('Booted up. microSWIFT configuration: \n'
+                    f'float ID: {self.ID}, payload type: {self.PAYLOAD_TYPE},'
+                    f' sensor type: {self.SENSOR_TYPE},'
+                    f'duty cycle length: {self.DUTY_CYCLE_LENGTH},'
+                    f'record window length: {self.RECORD_WINDOW_LENGTH}')
 
         return
         
@@ -123,3 +149,19 @@ class Config:
                 record_window_length, 
                 gps_sampling_frequency,
                 imu_sampling_frequency)
+
+def get_record_window_start_time(self):
+    """
+    Find the start of the current duty cycle as a datetime.
+    """
+    current_hour = datetime.utcnow().replace(minute=0, second=0)
+    for i in range(self.DUTY_CYCLES_PER_HOUR):
+        possible_start_time = current_hour + i*self.DUTY_CYCLE_LENGTH
+        if possible_start_time <= datetime.utcnow() \
+                                < possible_start_time + self.DUTY_CYCLE_LENGTH:
+            self.START_TIME = possible_start_time
+        else:
+            continue
+
+    return
+
