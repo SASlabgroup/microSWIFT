6fe84d551469ddebc7c0b62bc0d69fe6f6d066d1
Ping-Chun Lin <pcl1225@uw.edu>
2022-12-14 01:25:12 -0800

    minor style fix

--- microSWIFT/accoutrements/gps_module.py
+++ microSWIFT/accoutrements/gps_module.py
@@ -16,7 +16,7 @@ import RPi.GPIO as GPIO
 import pynmea2
 import serial
 
-from ..utils.config import Config
+from ..utils.configuration import Config
 
 # Set up module level logger
 logger = logging.getLogger('microSWIFT.'+__name__)
@@ -31,11 +31,12 @@ start_aud = config.getInt('GPS', 'startBaud')
 gps_timeout = config.getInt('GPS', 'timeout')
 burst_seconds = config.getInt('System', 'burst_seconds')
 gps_samples = gps_freq*burst_seconds
+ser=serial.Serial(config.gps_port,config.start_baud,timeout=1)
 ########################################################################
 
 class GPS:
     """
-    This class is for the GPS component on the RPi. 
+    This class is for the GPS component on the RPi.
     """
     def __init__(self, config):
         """_summary_
@@ -53,34 +54,35 @@ class GPS:
 
         logger.info('initializing GPS')
         try:
-            #start with GPS default baud
+            # start with GPS default baud
             logger.info("try GPS serial port at 9600")
-            ser=serial.Serial(config.gps_port,config.start_baud,timeout=1)
+            ser=serial.Serial(config.gps_port,config.start_baud,
+                              timeout=1)
         except Exception as err:
             logger.info('Serial port did not open')
             logger.info(err)
         try:
-            #set device baud rate to 115200
+            # set device baud rate to 115200
             ser.write('$PMTK251,115200*1F\r\n'.encode())
             sleep(1)
-            #switch ser port to 115200
+            # switch ser port to 115200
             ser.baudrate = config.baud
             logger.info("switching to %s on port %s" % (config.baud, config.gps_port))
-            #set output sentence to GPGGA and GPVTG, plus GPRMC once every 4 positions
-            #(See GlobalTop PMTK command packet PDF)
-            ser.write('$PMTK314,0,4,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*2C\r\n'.encode())
+            # set output sentence to GPGGA and GPVTG,plus GPRMC once every
+            # 4 positions(See GlobalTop PMTK command packet PDF)
+            ser.write('$PMTK314,0,4,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'
+                      '*2C\r\n'.encode())
             sleep(1)
-            #set interval to 250ms (4 Hz)
-            ser.write("$PMTK220,{}*29\r\n".format(config.GPS_SAMPLING_FREQ).encode()) 
-            #TODO: change sampling freq to milliseconds from HZ
+            # set interval to 250ms (4 Hz)
+            ser.write("$PMTK220,{}*29\r\n".format(config.GPS_SAMPLING_FREQ).encode())
+            # TODO: change sampling freq to milliseconds from HZ
             sleep(1)
         except Exception as err:
             logger.info('Failed to set baudrate and sampling frequency')
             logger.info(err)
-             
-        #read lines from GPS serial port and wait for fix
+        # read lines from GPS serial port and wait for fix
         try:
-            #loop until timeout dictated by gps_timeout value (seconds) or the gps is initialized
+            # loop until timeout dictated by gps_timeout value (seconds) or the gps is initialized
             while datetime.utcnow().minute + datetime.utcnow().second/60 < end_time and gps_initialized==False:
                 gps_initialized = self.__checkout__(gps_initialized)
             if gps_initialized == False:
@@ -105,7 +107,8 @@ class GPS:
                 logger.info(newline)
                 gpgga=pynmea2.parse(newline,check=True)
                 logger.info('GPS quality= {}'.format(gpgga.gps_qual))
-                #check gps_qual value from GPGGS sentence. 0=invalid,1=GPS fix,2=DGPS fix
+                # check gps_qual value from GPGGS sentence.
+                # 0=invalid,1=GPS fix,2=DGPS fix
                 if gpgga.gps_qual > 0:
                     logger.info('GPS fix acquired')
                     # Set gprmc line to False and enter while loop to read new lines until it gets the correct line
@@ -123,13 +126,12 @@ class GPS:
                                 nmea_date=gprmc.datestamp
                                 logger.info("nmea time: {}".format(nmea_time))
                                 logger.info("nmea date: {}".format(nmea_date))
-                                #set system time
+                                # set system time
                                 try:
                                     logger.info("setting system time from GPS: {0} {1}".format(nmea_date, nmea_time))
                                     os.system('sudo timedatectl set-timezone UTC')
                                     os.system('sudo date -s "{0} {1}"'.format(nmea_date, nmea_time))
                                     os.system('sudo hwclock -w')
-                                            
                                     # GPS is initialized
                                     logger.info("GPS initialized")
                                     init_status = True
@@ -138,14 +140,13 @@ class GPS:
                                 except Exception as err:
                                     logger.info(err)
                                     logger.info('error setting system time')
-                                    continue    
+                                    continue
                             except Exception as err:
                                 logger.info(err)
                                 logger.info('error parsing nmea sentence')
                                 continue
         sleep(1)
 
-    
     def __record__(self, end_time):
         """
         This is the function that record the gps component on RPi. The function takes in a timestamp.
@@ -173,10 +174,9 @@ class GPS:
                             newline=ser.readline().decode()
                             gps_out.write(newline)
                             gps_out.flush()
-                    
                             if "GPGGA" in newline:
                                 gpgga = pynmea2.parse(newline,check=True)   #grab gpgga sentence and parse
-                                #check to see if we have lost GPS fix, and if so, continue to loop start. a bad value will remain at this index
+                                # check to see if we have lost GPS fix, and if so, continue to loop start. a bad value will remain at this index
                                 if gpgga.gps_qual < 1:
                                     logger.info('lost GPS fix, sample not recorded. Waiting 10 seconds')
                                     logger.info('lost GPS fix, sample not recorded. Waiting 10 seconds')
@@ -186,7 +186,6 @@ class GPS:
                                 ipos+=1
                             elif "GPVTG" in newline:
                                 ivel+=1
-                            
                             # If the number of position and velocity samples is enough then and the loop
                             if ipos == gps_samples and ivel == gps_samples:
                                 break
@@ -194,16 +193,13 @@ class GPS:
                         logger.info('Ending GPS burst at {}'.format(datetime.now()))
                         logger.info('number of GPGGA samples = {}'.format(ipos))
                         logger.info('number of GPVTG samples = {}'.format(ivel))
-
                 except Exception as err:
                     logger.info(err, exc_info=True)
-
                 # Output logger information on samples
                 logger.info('Ending GPS burst at {}'.format(datetime.now()))
                 logger.info('number of GPGGA samples = {}'.format(ipos))
                 logger.info('number of GPVTG samples = {}'.format(ivel))
                 return GPSdataFilename
-
         # If GPS signal is not initialized exit
         else:
             logger.info("GPS not initialized, exiting")
@@ -213,7 +209,6 @@ class GPS:
 
             # Return the GPS filename to be read into the onboard processing
             return GPSdataFilename
-
 
     def to_uvz(self, gps_file):
         """
@@ -234,17 +229,12 @@ class GPS:
         ipos=0
         ivel=0
         GPS = {'east_west':None,'north_south':None,'up_down':None,'lat':None,'lon':None,'time':None}
-        
         # Define Constants 
         bad_value=999
-        
         # current year, month, date for timestamp creation; can also be obtained from utcnow()
         ymd = gps_file[-23:-14]
         # ymd = datetime.utcnow().strftime("%Y-%m-%d")
-
-
         with open(gps_file, 'r') as file:
-        
             for line in file:
                 if "GPGGA" in line:
                     gpgga = pynmea2.parse(line,check=True)   #grab gpgga sentence and parse
@@ -289,14 +279,10 @@ class GPS:
         # zSorted    = np.asarray(up_down)[sortInd].transpose()
         # latSorted  = np.asarray(lat)[sortInd].transpose()
         # lonSorted  = np.asarray(lon)[sortInd].transpose()
-
-
         # assign outputs to GPS dict
         GPS.update({'east_west':east_west,'north_south':north_south,'up_down':up_down,'lat':lat,'lon':lon,'time':time})
         # GPS.update({'east_west':uSorted,'north_south':vSorted,'up_down':zSorted,'lat':latSorted,'lon':lonSorted,'time':timeSorted})
-
         logger.info('GPGGA lines: {}'.format(len(lat)))
         logger.info('GPVTG lines: {}'.format(len(east_west)))
         logger.info('------------------------------------------')
-
         return GPS #east_west,north_south,up_down,lat,lon, time
